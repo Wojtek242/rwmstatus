5b271066992e00ce504a9bd6254b115467fe5891
Wojciech Kozlowski <wk@wojciechkozlowski.eu>
2018-12-30 21:36:33 +0800

    Remove unnecessary explicit 'static

--- src/config.rs
+++ src/config.rs
@@ -1,10 +1,10 @@
 //! # rwmstatus configuration
 
 /// Path to monitors.
-pub const HW_MON_PATH: &'static str = "/sys/devices/virtual/hwmon";
+pub const HW_MON_PATH: &str = "/sys/devices/virtual/hwmon";
 
 /// Path to power supply information.
-pub const BATT_PATH: &'static str = "/sys/class/power_supply";
+pub const BATT_PATH: &str = "/sys/class/power_supply";
 
 /// Additional time zones to display (short name, full name).
-pub const TZS: [(char, &'static str); 2] = [('A', "America/Buenos_Aires"), ('U', "UTC")];
+pub const TZS: [(char, &str); 2] = [('A', "America/Buenos_Aires"), ('U', "UTC")];
